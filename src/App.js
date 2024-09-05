be622c0ad051b30bb57b48f1638aa8f2930010bc
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-05 16:19:11 +0300

    ilk user gelene kadar loading ekranında kalınacak

--- src/App.js
+++ src/App.js
@@ -17,10 +17,18 @@ function App() {
   const [person, setPerson] = useState({});
   const [title, setTitle] = useState("name");
   const [userValue, setUserValue] = useState("");
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     getUser();
   }, []);
+
+  const handleAddUser = () => {
+    if (person) {
+      setUsers((prevUsers) => [...prevUsers, person]);
+    }
+  };
 
   const getUser = async () => {
     const response = await fetch(url);
@@ -29,6 +37,7 @@ function App() {
     setPerson(user);
     setTitle("My name is");
     setUserValue(`${user.name.first} ${user.name.last}`);
+    setLoading(false);
   };
 
   const handleValue = (e) => {
@@ -57,91 +66,104 @@ function App() {
   };
 
   return (
-    <main>
-      <div className="block bcg-orange">
-        <img src={cwSvg} alt="cw" id="cw" />
-      </div>
-      <div className="block">
-        <div className="container">
-          <img
-            src={person.picture?.large || defaultImage}
-            alt="random user"
-            className="user-img"
-          />
-          <p className="user-title">{title}</p>
-          <p className="user-value">{userValue}</p>
-          <div className="values-list">
-            <button
-              className="icon"
-              data-label="name"
-              onMouseEnter={handleValue}
-            >
-              <img src={womanSvg} alt="user" id="iconImg" />
-            </button>
-            <button
-              className="icon"
-              data-label="email"
-              onMouseEnter={handleValue}
-            >
-              <img src={mailSvg} alt="mail" id="iconImg" />
-            </button>
-            <button
-              className="icon"
-              data-label="age"
-              onMouseEnter={handleValue}
-            >
-              <img src={womanAgeSvg} alt="age" id="iconImg" />
-            </button>
-            <button
-              className="icon"
-              data-label="street"
-              onMouseEnter={handleValue}
-            >
-              <img src={mapSvg} alt="map" id="iconImg" />
-            </button>
-            <button
-              className="icon"
-              data-label="phone"
-              onMouseEnter={handleValue}
-            >
-              <img src={phoneSvg} alt="phone" id="iconImg" />
-            </button>
-            <button
-              className="icon"
-              data-label="password"
-              onMouseEnter={handleValue}
-            >
-              <img src={padlockSvg} alt="lock" id="iconImg" />
-            </button>
+    <>
+      {loading ? (
+        <h1 className="load">Loading...</h1>
+      ) : (
+        <main>
+          <div className="block bcg-orange">
+            <img src={cwSvg} alt="cw" id="cw" />
           </div>
-          <div className="btn-group">
-            <button className="btn" type="button">
-              new user
-            </button>
-            <button className="btn" type="button">
-              add user
-            </button>
-          </div>
+          <div className="block">
+            <div className="container">
+              <img
+                src={person.picture?.large || defaultImage}
+                alt="random user"
+                className="user-img"
+              />
+              <p className="user-title">{title}</p>
+              <p className="user-value">{userValue}</p>
+              <div className="values-list">
+                <button
+                  className="icon"
+                  data-label="name"
+                  onMouseEnter={handleValue}
+                >
+                  <img src={womanSvg} alt="user" id="iconImg" />
+                </button>
+                <button
+                  className="icon"
+                  data-label="email"
+                  onMouseEnter={handleValue}
+                >
+                  <img src={mailSvg} alt="mail" id="iconImg" />
+                </button>
+                <button
+                  className="icon"
+                  data-label="age"
+                  onMouseEnter={handleValue}
+                >
+                  <img src={womanAgeSvg} alt="age" id="iconImg" />
+                </button>
+                <button
+                  className="icon"
+                  data-label="street"
+                  onMouseEnter={handleValue}
+                >
+                  <img src={mapSvg} alt="map" id="iconImg" />
+                </button>
+                <button
+                  className="icon"
+                  data-label="phone"
+                  onMouseEnter={handleValue}
+                >
+                  <img src={phoneSvg} alt="phone" id="iconImg" />
+                </button>
+                <button
+                  className="icon"
+                  data-label="password"
+                  onMouseEnter={handleValue}
+                >
+                  <img src={padlockSvg} alt="lock" id="iconImg" />
+                </button>
+              </div>
+              <div className="btn-group">
+                <button className="btn" type="button" onClick={getUser}>
+                  new user
+                </button>
+                <button className="btn" type="button" onClick={handleAddUser}>
+                  add user
+                </button>
+              </div>
 
-          <table className="table">
-            <thead>
-              <tr className="head-tr">
-                <th className="th">Firstname</th>
-                <th className="th">Email</th>
-                <th className="th">Phone</th>
-                <th className="th">Age</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="body-tr"></tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Footer />
-      </div>
-    </main>
+              <table className="table">
+                <thead>
+                  <tr className="head-tr">
+                    <th className="th">Firstname</th>
+                    <th className="th">Email</th>
+                    <th className="th">Phone</th>
+                    <th className="th">Age</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {users.map((user, index) => (
+                    <tr key={index} className="body-tr">
+                      <td>{user.name.first}</td>
+                      <td>{user.email}</td>
+                      <td>{user.phone}</td>
+                      <td>{user.dob.age}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <Footer />
+          </div>
+        </main>
+      )}
+    </>
   );
 }
 
